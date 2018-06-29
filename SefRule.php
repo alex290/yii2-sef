3470aafd0a15cda3d322e961c9f4eccca3c3e760
Mazda Penza <mazda.penza@gmail.com>
2018-06-29 10:06:27 +0300

    Fix PHP 7.2 and Array link

--- SefRule.php
+++ SefRule.php
@@ -4,7 +4,8 @@ namespace alex290\sef;
 
 use alex290\sef\Sef;
 use yii\web\UrlRuleInterface;
-use yii\base\Object;
+use yii\base\BaseObject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,95 +17,99 @@ use yii\base\Object;
  *
  * @author art
  */
-class SefRule extends Object implements UrlRuleInterface {
+class SefRule extends BaseObject implements UrlRuleInterface {
+
     //put your code here
     public $connectionID = 'db';
     public $name;
- 
-    public function init(){
+
+    public function init() {
         if ($this->name === null) {
             $this->name = __CLASS__;
         }
     }
-    
-    public function createUrl($manager, $route, $params)
-    {
+
+    public function createUrl($manager, $route, $params) {
         //debug($route);
         //Определяем контроллеры, у которых к страницам нужно добавлять .html
-        $controller = explode('/',$route)[0]; //Получаем контроллер        
+        $controller = explode('/', $route)[0]; //Получаем контроллер        
         $html = '';
 
         //Если передаются параметры (напрмиер ?id=3&page=2) сохраняем в $link по-очереди
-        $link ='';
-        $page ='';
-        if(count($params)){
+        $link = '';
+        $page = '';
+        if (count($params)) {
             $link = "?";
             $page = false;
-            foreach ($params as $key => $value){
-                if($key == 'page'){
+            foreach ($params as $key => $value) {
+                if ($key == 'page') {
                     $page = $value;
                     continue;
                 }
-                $newValue = !is_array($value) ? $value : join($value, ',');
+                if (!is_array($value)) {
+                    $newValue = $value;
+                } else {
+                    $newValue = '';
+                }
                 $link .= "$key=$newValue&";
-                
             }
             $link = substr($link, 0, -1); //удаляем последний символ (&)
         }
         //Из БД получаем строку со ссылкой на которую нужно будет поменять
-        $sef = Sef::find()->where(['link' => $route.$link])->one();
+        $sef = Sef::find()->where(['link' => $route . $link])->one();
 
-        if ($sef){
+        if ($sef) {
             //Если есть - добавляем пагинацию в конец (?page=2)
-            if ($page) return $sef->link_sef."?page=$page";
-            else return $sef->link_sef;
+            if ($page)
+                return $sef->link_sef . "?page=$page";
+            else
+                return $sef->link_sef;
         }
         return false;
     }
-    
-    public function parseRequest($manager, $request)
-    {
+
+    public function parseRequest($manager, $request) {
         //Получаем URL
-        $pathInfo = $request->getPathInfo();    
+        $pathInfo = $request->getPathInfo();
 
         //Получаем 1 часть, до слэша, если есть
-        $alias = explode('/',$pathInfo)[0];
+        $alias = explode('/', $pathInfo)[0];
         //Если на конце .html, то убираем для поиска в БД
-        $alias_small = str_replace(".html","",$alias);
+        $alias_small = str_replace(".html", "", $alias);
 
         //не выводить .html для указанных URL (первая часть алиаса)
         $not_html = [
-            'category','article','posts','notes'
+            'category', 'article', 'posts', 'notes'
         ];
 
         /*
-        * Проверяем наличие URL (до слэша) в $not_html
-        * Если есть, то в URL не должно быть окончание .html
-        * $exception = true разрешает поиск URL в БД
-        */     
+         * Проверяем наличие URL (до слэша) в $not_html
+         * Если есть, то в URL не должно быть окончание .html
+         * $exception = true разрешает поиск URL в БД
+         */
 
 
-            //получаем из БД данные по строке содержащей заданный алиас
-            $sef  = Sef::find()->where(['link_sef' => $pathInfo])->one();
+        //получаем из БД данные по строке содержащей заданный алиас
+        $sef = Sef::find()->where(['link_sef' => $pathInfo])->one();
 
-            if($sef){
-                //Разбивает строку типа post/view?id=5 на массив по разделителю
-                $link_data = explode('?',$sef->link);
-                //берем только первую часть без параметров (контроллер/действие)
-                $route = $link_data[0]; 
-                $params = array();
-                //если есть параметры - вставляем их 
-                if(isset($link_data[1])){
-                    $temp = explode('&',$link_data[1]);
-                    foreach($temp as $t){
-                        $t = explode('=', $t);
-                        $params[$t[0]] = $t[1];
-                    }
+        if ($sef) {
+            //Разбивает строку типа post/view?id=5 на массив по разделителю
+            $link_data = explode('?', $sef->link);
+            //берем только первую часть без параметров (контроллер/действие)
+            $route = $link_data[0];
+            $params = array();
+            //если есть параметры - вставляем их 
+            if (isset($link_data[1])) {
+                $temp = explode('&', $link_data[1]);
+                foreach ($temp as $t) {
+                    $t = explode('=', $t);
+                    $params[$t[0]] = $t[1];
                 }
-                //$route - контроллер/действие
-                //$params - параметры
-                return [$route, $params];
             }
+            //$route - контроллер/действие
+            //$params - параметры
+            return [$route, $params];
+        }
 
         return false;
     }
